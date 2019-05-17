f17132c8b6f29e34a0042640c4fa40ceee602899
reduviidae <wiljago@gmail.com>
2019-05-17 13:54:41 -0400

    ready to seed

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,4 +1,21 @@
 class BreweriesController < ApplicationController
-  
+  def index
+    @breweries = Brewery.all
+    render :json => @breweries
+  end
+
+  # def create
+  #   @brewery = Brewry.create(brewery_params)
+  #   if @brewery.save
+  #     render json: @brewery
+  #   else
+  #     render json: { error: `failed to create brewery`}, status: :not_acceptible
+  #   end
+  # end
+  #
+  # private
+  # def brewery_params
+  #   params.require(:brewery).permit(:name, :brewery_type, :street, :city, :state, :postal_code, :country, :longtitude, :latitude, :phone, :website_url)
+  # end
 
 end
