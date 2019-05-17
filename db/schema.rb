c993e8a76de274e91f372e2bbd3f1b78ae42be79
reduviidae <wiljago@gmail.com>
2019-05-17 14:47:59 -0400

    switched to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_17_170803) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.string "brewery_type"
